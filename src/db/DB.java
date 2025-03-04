a8fc7b1ac409d719d619b5f4da64399b84d91171
Camille de Oliveira <camilleoliveira324@gmail.com>
2025-03-04 17:57:10 -0300

    Project created

---
+++ src/db/DB.java
@@ -0,0 +1,64 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException sqle) {
+                throw new DbException(sqle.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static Connection closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException sqle) {
+                throw new DbException(sqle.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException io) {
+            throw new DbException(io.getMessage());
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
