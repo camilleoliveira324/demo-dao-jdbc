57075860eda8740e901f81048a0a7f838c8b2c99
Camille de Oliveira <camilleoliveira324@gmail.com>
2025-03-05 17:25:12 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,10 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(1, "Camille", "ca@gmail.com", new Date() , 5500.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        
+        Seller seller = sellerDao.findById(3);
 
-        System.out.println(obj);
         System.out.println(seller);
     }
 }
